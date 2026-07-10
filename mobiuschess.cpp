792ebbe0affee4a4178e57a9bc26f5d039550c45
edwin1in <edwinzhanminglin@gmail.com>
2026-07-09 17:00:28 -0700

    feat: add GLFW window

---
+++ mobiuschess.cpp
@@ -0,0 +1,33 @@
+﻿#include "mobiuschess.h"
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "window", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    return 0;
+}
+
